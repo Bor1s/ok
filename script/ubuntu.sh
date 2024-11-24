98fc610b7fb2c179d60fb025fa5436121e334487
Borys <boris.bbk@gmail.com>
2024-11-24 10:41:36 +0100

    Add build-essentials for Ubuntu Rust setup

--- script/ubuntu.sh
+++ script/ubuntu.sh
@@ -82,6 +82,7 @@ echo "Installing tools"
 
 # Update package lists and install prerequisites
 sudo apt-get update && sudo apt-get upgrade -y
+sudo apt install build-essential
 
 # Install tools
 check_and_install "git" "sudo apt-get install -y git"
