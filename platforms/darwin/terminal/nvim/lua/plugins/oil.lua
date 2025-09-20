7102f722669568d434f7883ca8d245de70374346
Borys <boris.bbk@gmail.com>
2025-09-20 10:53:08 +0200

    Update mini.icons to a new git repo path

--- platforms/darwin/terminal/nvim/lua/plugins/oil.lua
+++ platforms/darwin/terminal/nvim/lua/plugins/oil.lua
@@ -5,6 +5,6 @@ return {
     ---@type oil.SetupOpts
     opts = {},
     -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   },
 }
